7603a20faf3b139ec47f53af88bba2bc94efa355
Prathamesh Kawale <pkawale@apple.com>
2016-12-09 23:36:26 -0800

    FindTwoNumbersSummingToTarget

---
+++ src/main/java/arrays/FindTwoNumbersSummingToTarget.java
@@ -0,0 +1,13 @@
+package arrays;
+
+public class FindTwoNumbersSummingToTarget {
+	
+	public void test(int[] array){
+		
+	}
+
+	public static void main(String[] args) {
+		int[] array = { 2, 4, 6, 8, 10, 11 };
+	}
+
+}
