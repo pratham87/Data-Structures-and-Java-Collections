512f384979c9bfa264524c1c12a4eb503e5f3df8
Prathamesh Kawale <kawa5@live.com>
2016-12-24 16:19:44 -0800

    implementing stack using linked list - push method

--- src/main/java/linkedList/StackUsingLinkedList.java
+++ src/main/java/linkedList/StackUsingLinkedList.java
@@ -1,25 +1,52 @@
 package linkedList;
 
 public class StackUsingLinkedList {
-	
-	public void push(){
-		
-	}
-	
-	public void pop(){
-		
-	}
-	
-	public void getSize(){
-		
-	}
-	
-	public void displayStack(){
-		
+
+	Node head;
+
+	public void push(Node node) {
+		if (head == null) {
+			head = node;
+		} else {
+			node.next = head;
+			head = node;
+		}
 	}
 
-	
+	public void pop() {
+
+	}
+
+	public void getSize() {
+
+	}
+
+	public void displayStack() {
+		Node temp = head;
+		while (temp != null) {
+			System.out.print(temp.data + " --> ");
+			temp = temp.next;
+		}
+	}
+
 	public static void main(String[] args) {
+		StackUsingLinkedList list = new StackUsingLinkedList();
+		list.head = new Node(1);
+		list.head.next = new Node(2);
+		list.head.next.next = new Node(3);
+		list.head.next.next.next = new Node(4);
+		list.head.next.next.next.next = new Node(5);
+
+		System.out.println("Original List: ");
+		list.displayStack();
+
+		System.out.println("\n\nModified List: ");
+		list.push(new Node(7));
+		list.displayStack();
+		
+		System.out.println("\n\nModified List: ");
+		list.push(new Node(8));
+		list.displayStack();
 
 	}
 
