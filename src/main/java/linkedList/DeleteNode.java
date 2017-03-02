b6b3bde2ad13cee280068702d8f656059a518943
Prathamesh Kawale <kawa5@live.com>
2017-03-01 21:30:34 -0800

    fixed deleteNode

--- src/main/java/linkedList/DeleteNode.java
+++ src/main/java/linkedList/DeleteNode.java
@@ -3,7 +3,6 @@ package linkedList;
 public class DeleteNode {
 
 	Node head;
-	Node temp = head;
 
 	public void deleteNode(int position) {
 
@@ -11,22 +10,29 @@ public class DeleteNode {
 			return;
 		}
 
+		Node temp = head;
+
 		// Position 0 is head
-		if (position == 0) {
+		if (position == 1) {
 			head = temp.next;
 			return;
 		}
 
-		for (int i = 0; i < position; i++) {
-			temp = temp.next;
+		for (int i = 1; i < position - 1; i++) {
+			if (temp != null) {
+				temp = temp.next;
+			}
+
 		}
 
 		if (temp == null || temp.next == null) {
 			return;
 		}
-		
-		Node next = temp.next.next;
-		temp.next = next; 
+
+		// Node copy = temp.next.next;
+		// temp.next = copy;
+
+		temp.next = temp.next.next;
 
 	}
 
@@ -44,13 +50,13 @@ public class DeleteNode {
 		list.head.next = new Node(2);
 		list.head.next.next = new Node(3);
 		list.head.next.next.next = new Node(4);
-		
+
 		System.out.println("Before deleting the Node:");
 		list.print();
-		
+
 		list.deleteNode(3);
-		
-		System.out.println("After deleting the Node:");
+
+		System.out.println("\n\nAfter deleting the Node:");
 		list.print();
 	}
 
