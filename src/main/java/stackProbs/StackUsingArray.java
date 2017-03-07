9a16cb62aa09317097422d3e8a1a532fefccc14d
Prathamesh Kawale <kawa5@live.com>
2017-03-07 14:40:44 -0800

    stack using array

--- src/main/java/stackProbs/StackUsingArray.java
+++ src/main/java/stackProbs/StackUsingArray.java
@@ -1,17 +1,56 @@
 package stackProbs;
 
 public class StackUsingArray {
-	
-	public void push(){
-		
-	}
-	
-	public void pop(){
-		
+
+	int top;
+	int a[] = new int[10];
+
+	public StackUsingArray() {
+		top = -1;
 	}
 
-	public static void main(String[] args) {
-		
+	public boolean isEmpty() {
+		return (top < 0);
+	}
+
+	boolean push(int x) {
+		if (top >= a.length) {
+			System.out.println("Stack Overflow");
+			return false;
+		} else {
+			a[++top] = x;
+			return true;
+		}
+	}
+
+	public int pop() {
+		if (top < 0) {
+			System.out.println("Stack Underflow");
+			return 0;
+		} else {
+			return a[top--];
+		}
+	}
+
+	public void printList() {
+		System.out.println("List is:");
+		for (int i = 0; i < a.length; i++) {
+			System.out.print(a[i] + " ");
+		}
+	}
+
+	public static void main(String args[]) {
+		StackUsingArray s = new StackUsingArray();
+
+		s.push(20);
+		s.push(30);
+
+		System.out.println(s.pop() + " Popped from stack");
+
+		s.push(10);
+		s.push(50);
+
+		s.printList();
 	}
 
 }
