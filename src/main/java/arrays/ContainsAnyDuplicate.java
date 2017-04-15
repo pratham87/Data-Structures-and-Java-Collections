04c96c2182e8cf53185221954aa4284490d403c7
Prathamesh Kawale <kawa5@live.com>
2017-04-14 17:13:49 -0700

    ContainsAnyDuplicate

---
+++ src/main/java/arrays/ContainsAnyDuplicate.java
@@ -0,0 +1,31 @@
+package arrays;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/*
+ Find if the array contains any duplicates. 
+ 
+ Algorithm:
+ 
+ 1. Use Set to add elements.
+ 2. Set's add method will return false if the element already exists else it will return true and will add the element.
+ */
+public class ContainsAnyDuplicate {
+
+	public boolean containsDuplicate(int[] nums) {
+		Set<Integer> set = new HashSet<Integer>();
+		for (int num : nums) {
+			if (!set.add(num)) {
+				return true;
+			}
+		}
+
+		return false;
+	}
+
+	public static void main(String[] args) {
+
+	}
+
+}
