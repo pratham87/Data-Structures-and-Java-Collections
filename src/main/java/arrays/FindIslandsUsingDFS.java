631df1d19a61d52cf4ad17a62822ba4a14a4d614
Prathamesh Kawale <kawa5@live.com>
2017-04-27 21:38:02 -0700

    Update FindIslandsUsingDFS.java

--- src/main/java/arrays/FindIslandsUsingDFS.java
+++ src/main/java/arrays/FindIslandsUsingDFS.java
@@ -2,9 +2,13 @@ package arrays;
 
 import java.util.Stack;
 
-/*
- 		0  1  0  1  0
- 		0  0  1  1  1
+/* 
+Given a 2D array binaryMatrix of 0s and 1s, implement a function getNumberOfIslands that returns the number of islands of 1s in binaryMatrix.
+An island is defined as a group of adjacent values that are all 1s. 
+A cell in binaryMatrix is considered adjacent to another cell if they are next to each either on the same row or column. 
+Note that two values of 1 are not part of the same island if they’re sharing only a mutual “corner” (i.e. they are diagonally neighbors).
+ 	0  1  0  1  0
+        0  0  1  1  1
         1  0  0  1  0
         0  1  1  0  0
         1  0  1  0  1 
