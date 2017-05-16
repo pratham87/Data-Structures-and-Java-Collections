c79876a0c6cb91176b090bdfcc0f13d245959c27
Prathamesh Kawale <kawa5@live.com>
2017-05-16 14:35:44 -0700

    nBitString

---
+++ src/main/java/arrays/nBitString.java
@@ -0,0 +1,43 @@
+package arrays;
+
+import java.util.Arrays;
+
+/*
+ 	Gen­er­ate All Strings of n bits, con­sider A[0..n-1] is an array of size n.
+
+	Exam­ple :
+	
+	n = 3
+	Output:
+	[0, 0, 0]    [1, 0, 0]    [0, 1, 0]    [1, 1, 0]
+	
+	[0, 0, 1]    [1, 0, 1]     [0, 1, 1]    [1, 1, 1] 
+ */
+
+public class nBitString {
+
+	int[] arr;
+
+	public nBitString(int n) {
+		arr = new int[n];
+	}
+
+	public void nBits(int n) {
+		if (n <= 0) {
+			System.out.println(Arrays.toString(arr));
+		} else {
+			arr[n - 1] = 0;
+			nBits(n - 1);
+			arr[n - 1] = 1;
+			nBits(n - 1);
+		}
+
+	}
+
+	public static void main(String[] args) {
+		int n = 3;
+		nBitString nb = new nBitString(n);
+		nb.nBits(n);
+	}
+
+}
