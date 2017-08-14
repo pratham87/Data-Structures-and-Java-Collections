e08c88ad92d424e561386d3c1a3f5882ad12d3b5
Prathamesh Kawale <kawa5@live.com>
2017-08-14 15:17:46 -0700

    Min Max

--- src/main/java/arrays/MaxAndMin.java
+++ src/main/java/arrays/MaxAndMin.java
@@ -1,25 +1,28 @@
 package arrays;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class MaxAndMin {
-	static int max;
-	static int index;
 
-	public static int findMax(int[] array) {
-		max = array[0];
+	public static List<Integer> findMaxMin(int[] array) {
+		int max = array[0];
+		int min = array[0];
 
-		for (int i = 0; i < array.length - 1; i++) {
-			if (max < array[i + 1]) {
-				max = array[i + 1];
-				index = i+2;
+		for (int i = 1; i < array.length - 1; i++) {
+			if (max < array[i]) {
+				max = array[i];
+			} else if (min > array[i]) {
+				min = array[i];
 			}
 		}
-		return max;
+		return Arrays.asList(min, max);
 	}
 
 	public static void main(String s[]) {
 		int[] array = new int[] { 5, 4, 99, 110, 8, 9, 109, 10 };
 
-		System.out.println(findMax(array));
-		System.out.println("Index of the max element is: " + index);
+		System.out.println("Min: " + findMaxMin(array).get(0));
+		System.out.println("Max: " + findMaxMin(array).get(1));
 	}
 }
